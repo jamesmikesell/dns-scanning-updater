7161e4982ed3b5db81e7868075a3f19aa4e5d7dc
James Mikesell <james.mikesell@gmail.com>
2024-09-29 10:19:41 -0400

    waiting until ttl expires to re-check a recently changed dns entry

--- runner.ts
+++ runner.ts
@@ -6,28 +6,45 @@ export class Runner {
 
   static async run(configs: CloudflareConfig[]) {
     for (const singleConfig of configs) {
-      setInterval(() => {
-        void this.runOnce(singleConfig)
-      }, singleConfig.updateIntervalSeconds * 1000);
+      let lastUpdated: Date;
+      if (await this.runOnce(singleConfig))
+        lastUpdated = new Date()
 
-      void this.runOnce(singleConfig)
+      let updateIntervalMs = singleConfig.updateIntervalSeconds * 1000;
+      setInterval(async () => {
+        let updatedSecondsAgo: number | undefined;
+        if (lastUpdated)
+          updatedSecondsAgo = (Date.now() - lastUpdated.getTime()) / 1000
+
+        if (!updatedSecondsAgo || updatedSecondsAgo > singleConfig.ttl) {
+          if (await this.runOnce(singleConfig))
+            lastUpdated = new Date()
+        } else {
+          console.log(`${singleConfig.fullSubDomainName} was updated ${updatedSecondsAgo.toFixed(0)}s ago, waiting until TTL of ${singleConfig.ttl}s expires to recheck.`)
+        }
+      }, updateIntervalMs);
+
     }
   }
 
 
-  private static async runOnce(config: CloudflareConfig): Promise<void> {
+  private static async runOnce(config: CloudflareConfig): Promise<boolean> {
     let start = Date.now();
-    await this.tryUpdate(config);
+    let updated = await this.tryUpdate(config);
     let remainingSeconds = config.updateIntervalSeconds - ((Date.now() - start) / 1000)
-    console.log(`Waiting ${remainingSeconds.toFixed(0)}s to try updating ${config.fullSubDomainName} again`)
+    console.log(`Waiting ${remainingSeconds.toFixed(0)}s to try recheck ${config.fullSubDomainName}`)
+    return updated;
   }
 
-  private static async tryUpdate(config: CloudflareConfig) {
+
+  /** Returns true of the DNS record is actually updated */
+  private static async tryUpdate(config: CloudflareConfig): Promise<boolean> {
     console.log(`Running updater for ${config.fullSubDomainName}`);
 
     const host = config.lanHostName;;
     const lanIpAddress = await IpResolver.getIPAddressOfLanName(host);
 
+    let updated = false;
     if (lanIpAddress) {
       console.log(`LAN IP address of ${host}: ${lanIpAddress}`);
 
@@ -36,14 +53,19 @@ export class Runner {
       if (currentDnsIpAddress !== lanIpAddress) {
         console.log(`LAN IP ${lanIpAddress} differs from DNS IP ${currentDnsIpAddress}`)
 
-        CloudflareDNSUpdater.updateDns({
-          apiToken: config.cloudflareApiToken,
-          domain: config.domainName,
-          subdomain: config.fullSubDomainName,
-          newIp: lanIpAddress,
-          ttl: config.ttl,
-        }).then(() => console.log('DNS update completed.'))
-          .catch(err => console.error('Error during DNS update:', err));
+        try {
+          await CloudflareDNSUpdater.updateDns({
+            apiToken: config.cloudflareApiToken,
+            domain: config.domainName,
+            subdomain: config.fullSubDomainName,
+            newIp: lanIpAddress,
+            ttl: config.ttl,
+          })
+          console.log('DNS update completed.')
+          updated = true;
+        } catch (error) {
+          console.error('Error during DNS update:', error)
+        }
       } else {
         console.log(`LAN IP ${lanIpAddress} same as DNS IP ${currentDnsIpAddress}`)
       }
@@ -51,6 +73,8 @@ export class Runner {
     } else {
       console.log(`Could not resolve the IP address of ${host}`);
     }
+
+    return updated;
   }
 
 }
